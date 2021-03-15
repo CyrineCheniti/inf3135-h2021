e8da38601b5f77c2ba56a79524d62a3c0bcf541c
JocelynBedard <bedard.jocelyn@courrier.uqam.ca>
2021-03-15 16:42:08 -0400

    resultats

--- cmdline.c
+++ cmdline.c
@@ -6,7 +6,7 @@
 int cmdline(char input){
    int codeRetour=0;
    switch(input){
-    case 'd': 
+    case 'd':
     codeRetour =3;
     break;
     case 'c':
@@ -33,9 +33,9 @@ int main(int argc, char *argv[]){
      bool check=false;
      for (int i=1; i<argc; i++){
         if (strcmp(argv[i],"-c")==0){
-              check=true; 
+              check=true;
               if (strlen(argv[i+1])!=12){
-                    reslt=cmdline('c');  
+                    reslt=cmdline('c');
               }
         } else if (strcmp(argv[i],"-d")==0){
               if (strcmp(argv[i+1],"INC")!=0 &&  strcmp(argv[i+1],"DEC")!=0){
@@ -44,14 +44,13 @@ int main(int argc, char *argv[]){
         } else if (strcmp(argv[i],"-v")==0){
               if(!numeric(argv[i+1])){
                    reslt=cmdline('v');
-             } 
+             }
         } else if (i==argc-1 && check==false){
-                   reslt=cmdline('c');                   
+                   reslt=cmdline('c');
         }
-        
+
      }
      printf("\n%d\n",reslt);
      return reslt;
-       
-}
 
+}
