dbfd6930cd4e7b5caaf9f58b4b93185656389117
Cyrine Cheniti <cheniti.cyrine@courrier.uqam.ca>
2021-03-21 15:20:04 -0400

    corriger les erreurs

--- cmdline.c
+++ cmdline.c
@@ -31,23 +31,26 @@ bool numeric(char *str){
 int main(int argc, char *argv[]){
      int reslt=0;
      bool check=false;
+     if (argc ==1){
+         reslt=cmdline('c');
+     }
      for (int i=1; i<argc; i++){
         if (strcmp(argv[i],"-c")==0){
               check=true;
-              if (strlen(argv[i+1])!=12){
+              if (i+1==argc || strlen(argv[i+1])!=12){
                     reslt=cmdline('c');
               }
         } else if (strcmp(argv[i],"-d")==0){
-              if (strcmp(argv[i+1],"INC")!=0 &&  strcmp(argv[i+1],"DEC")!=0){
+              if (i+1==argc || (strcmp(argv[i+1],"INC")!=0 && strcmp(argv[i+1],"DEC")!=0) ){
                     reslt=cmdline('d');
               }
         } else if (strcmp(argv[i],"-v")==0){
-              if(!numeric(argv[i+1])){
+              if(i+1==argc || !numeric(argv[i+1])){
                    reslt=cmdline('v');
              }
         } else if (i==argc-1 && check==false){
                    reslt=cmdline('c');
-        }
+        } 
 
      }
      printf("\n%d\n",reslt);
